a61edbe32503891abefe859f403ffe1474fa94f2
hamseungchan <head0618@naver.com>
2024-03-08 08:43:24 +0900

    [init] : project 생성

---
+++ piepay-api/src/test/java/com/pay/pie/PieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pay.pie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
