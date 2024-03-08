373224d68e3372c661f9385cc1f19090986a37e7
hamseungchan <head0618@naver.com>
2024-03-08 12:36:58 +0900

    [feat] : Error 초기 설정

---
+++ piepay-api/src/main/java/com/pay/pie/global/exception/GlobalControllerAdvice.java
@@ -0,0 +1,30 @@
+package com.pay.pie.global.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.pay.pie.global.exception.dto.ErrorResponse;
+
+@RestControllerAdvice
+public class GlobalControllerAdvice {
+
+	@ExceptionHandler
+	public ResponseEntity<ErrorResponse> handleCustomBaseExceptionHandler(GlobalException ex) {
+		ErrorCode errorCode = ex.getErrorCode();
+		ErrorResponse errorResponse = ErrorResponse.of()
+			.code(errorCode).build();
+		return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorCode.getStatus()));
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponse> handleMethodValidation(MethodArgumentNotValidException ex) {
+		ErrorResponse errorResponse = ErrorResponse.of()
+			.code(ErrorCode.NOT_VALID_ERROR)
+			.message(ex.getMessage())
+			.build();
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	}
+}
