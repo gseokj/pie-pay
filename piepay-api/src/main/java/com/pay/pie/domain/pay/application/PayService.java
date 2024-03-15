a8efe5c1317b8e6a1b9cc7827fa31ed97dba18ef
류지수 <onyourown793@gmail.com>
2024-03-15 16:19:45 +0900

    [feat]: getReceipt를 CompletedPay로 이동

--- piepay-api/src/main/java/com/pay/pie/domain/pay/application/PayService.java
+++ piepay-api/src/main/java/com/pay/pie/domain/pay/application/PayService.java
@@ -1,7 +1,4 @@
 package com.pay.pie.domain.pay.application;
 
-import com.pay.pie.domain.order.dto.response.ReceiptRes;
-
 public interface PayService {
-	ReceiptRes getReceipt(Long payId);
 }
--- piepay-api/src/main/java/com/pay/pie/domain/pay/application/PayServiceImpl.java
+++ piepay-api/src/main/java/com/pay/pie/domain/pay/application/PayServiceImpl.java
@@ -3,11 +3,7 @@ package com.pay.pie.domain.pay.application;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.pay.pie.domain.order.dao.OrderRepository;
-import com.pay.pie.domain.order.dto.response.ReceiptRes;
-import com.pay.pie.domain.order.entity.Order;
 import com.pay.pie.domain.pay.dao.PayRepository;
-import com.pay.pie.domain.pay.entity.Pay;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,18 +15,5 @@ import lombok.extern.slf4j.Slf4j;
 public class PayServiceImpl implements PayService {
 
 	private final PayRepository payRepository;
-	private final OrderRepository orderRepository;
-	// private final JPAQueryFactory query;
 
-	@Override
-	@Transactional(readOnly = true)
-	public ReceiptRes getReceipt(Long payId) {
-		Order order = orderRepository.findByPayId(payId);
-		Pay pay = payRepository.findById(payId).orElseThrow(
-			() -> new IllegalArgumentException("해당 Pay 없음")
-		);
-
-		return ReceiptRes.of(order, pay);
-		// return null;
-	}
 }
