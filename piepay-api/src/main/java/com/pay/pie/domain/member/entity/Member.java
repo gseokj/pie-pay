ecd15292515b08e4d39162b1a6e6d0828d51e8d8
hamseungchan <head0618@naver.com>
2024-03-15 15:30:01 +0900

    S10P22A402-69[feat] : Oauth2 Handler 구현

--- piepay-api/src/main/java/com/pay/pie/domain/member/entity/Member.java
+++ piepay-api/src/main/java/com/pay/pie/domain/member/entity/Member.java
@@ -1,15 +1,30 @@
 package com.pay.pie.domain.member.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
 @Table(name = "member")
 public class Member {
 	@Id
@@ -21,8 +36,8 @@ public class Member {
 	private String payPassword;
 
 	@Size(max = 11)
-	@Column(name = "Field3", length = 11)
-	private String field3;
+	@Column(name = "phone", length = 11)
+	private String phone;
 
 	@Size(max = 30)
 	@NotNull
@@ -33,4 +48,13 @@ public class Member {
 	@NotNull
 	@Column(name = "email", nullable = false, length = 30)
 	private String email;
+
+	@ElementCollection(fetch = FetchType.LAZY)
+	@Enumerated(EnumType.STRING)
+	@CollectionTable(name = "member_role", joinColumns = @JoinColumn(name = "member_id"))
+	private Set<MemberRole> role = new HashSet<>();
+
+	public String getUserRole() {
+		return role.toString();
+	}
 }
