6f206b17735d55715fa92a22b973fee3023a5091
류지수 <onyourown793@gmail.com>
2024-03-15 09:24:24 +0900

    S10P22A402-116 [feat]:Pay Entity enum 생성

--- piepay-api/src/main/java/com/pay/pie/domain/pay/entity/Pay.java
+++ piepay-api/src/main/java/com/pay/pie/domain/pay/entity/Pay.java
@@ -5,6 +5,8 @@ import com.pay.pie.domain.meet.entity.Meet;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,14 +15,17 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Entity
 @Table(name = "pay")
 public class Pay extends BaseEntity {
+
+	public enum PayStatus {
+		OPEN, ING, COMPLETE, CLOSE
+	}
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "pay_id")
@@ -33,7 +38,8 @@ public class Pay extends BaseEntity {
 
 	@NotNull
 	@Column(name = "pay_status", nullable = false)
-	private Boolean payStatus = false;
+	@Enumerated(EnumType.STRING)
+	private PayStatus payStatus;
 
 	@NotNull
 	@Column(name = "opener_id", nullable = false)
