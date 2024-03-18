a4e5300d10073988ceaed8631ea71082055c9dee
류지수 <onyourown793@gmail.com>
2024-03-18 11:38:13 +0900

    [fix]:MemberRepository 중복 제거

--- piepay-api/src/main/java/com/pay/pie/domain/member/dao/MemberRepository.java
+++ piepay-api/src/main/java/com/pay/pie/domain/member/dao/MemberRepository.java
@@ -1,5 +1,7 @@
 package com.pay.pie.domain.member.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import com.pay.pie.domain.member.entity.Member;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findByEmail(String email);
 }
