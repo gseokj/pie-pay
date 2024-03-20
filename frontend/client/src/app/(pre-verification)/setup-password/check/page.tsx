0776e41f3ee6c67350c9c288199e96393e0c04cb
CocoIsCat <demy176@naver.com>
2024-03-20 17:27:49 +0900

    [feat] : 비밀번호 확인 구현

--- frontend/client/src/app/(pre-verification)/setup-password/check/page.tsx
+++ frontend/client/src/app/(pre-verification)/setup-password/check/page.tsx
@@ -1,7 +1,7 @@
+'use client';
+
 import VirtualKeboard from '@/app/_component/VirtualKeboard';
 import * as styles from '@/styles/setpassword/setPassword.css';
-import Image from 'next/image';
-import dot from '@/assets/icons/dot.svg';
 
 export default function SimplePasswordCheck() {
   return (
@@ -12,7 +12,7 @@ export default function SimplePasswordCheck() {
           비밀번호를 다시 한번 입력하세요
         </div>
       </div>
-      <div>
+      <div className={styles.passwordWrapper}>
         <ul className={styles.passwordScreen}>
           <li className={styles.dotWrapper}>
             <div className={styles.dotAfterInput}></div>
