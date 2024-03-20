9615082ba970a872f7a6f9da2148f921688d54c3
CocoIsCat <demy176@naver.com>
2024-03-20 17:27:13 +0900

    [feat] : 비밀번호 설정 페이지 구현

--- frontend/client/src/app/(pre-verification)/setup-password/init/page.tsx
+++ frontend/client/src/app/(pre-verification)/setup-password/init/page.tsx
@@ -1,3 +1,40 @@
-export default function SimplePasswordInit() {
-  return <>비밀번호 설정</>;
+'use client';
+
+import VirtualKeboard from '@/app/_component/VirtualKeboard';
+import * as styles from '@/styles/setpassword/setPassword.css';
+
+export default function SimplePasswordSet() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.textBox}>
+        <div className={styles.title}>간편 비밀번호 설정</div>
+        <div className={styles.description}>
+          새로 설정할 간편 비밀번호를 입력하세요
+        </div>
+      </div>
+      <div className={styles.passwordWrapper}>
+        <ul className={styles.passwordScreen}>
+          <li className={styles.dotWrapper}>
+            <div className={styles.dotAfterInput}></div>
+          </li>
+          <li className={styles.dotWrapper}>
+            <div className={styles.dotAfterInput}></div>
+          </li>
+          <li className={styles.dotWrapper}>
+            <div className={styles.dotAfterInput}></div>
+          </li>
+          <li className={styles.dotWrapper}>
+            <div className={styles.dotBeforeInput}></div>
+          </li>
+          <li className={styles.dotWrapper}>
+            <div className={styles.dotBeforeInput}></div>
+          </li>
+          <li className={styles.dotWrapper}>
+            <div className={styles.dotBeforeInput}></div>
+          </li>
+        </ul>
+      </div>
+      <VirtualKeboard />
+    </div>
+  );
 }
