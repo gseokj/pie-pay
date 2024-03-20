2c618837875f4fb4484c9b956b475dc0d1d75e1b
CocoIsCat <demy176@naver.com>
2024-03-20 14:21:56 +0900

    [feat] : 비밀번호 보여주기 창 구현

--- frontend/client/src/app/(pre-verification)/setup-password/check/page.tsx
+++ frontend/client/src/app/(pre-verification)/setup-password/check/page.tsx
@@ -1,7 +1,40 @@
+import VirtualKeboard from '@/app/_component/VirtualKeboard';
+import * as styles from '@/styles/setpassword/setPassword.css';
+import Image from 'next/image';
+import dot from '@/assets/icons/dot.svg';
+
 export default function SimplePasswordCheck() {
   return (
-    <div>
-      <h1>비밀번호 왜 안 바뀌냐</h1>
+    <div className={styles.container}>
+      <div className={styles.textBox}>
+        <div className={styles.title}>간편 비밀번호 확인</div>
+        <div className={styles.description}>
+          비밀번호를 다시 한번 입력하세요
+        </div>
+      </div>
+      <div>
+        <ul className={styles.passwordScreen}>
+          <li className={styles.dotWrapper}>
+            <div className={styles.dotAfterInput}></div>
+          </li>
+          <li className={styles.dotWrapper}>
+            <div className={styles.dotAfterInput}></div>
+          </li>
+          <li className={styles.dotWrapper}>
+            <div className={styles.dotAfterInput}></div>
+          </li>
+          <li className={styles.dotWrapper}>
+            <div className={styles.dotBeforeInput}></div>
+          </li>
+          <li className={styles.dotWrapper}>
+            <div className={styles.dotBeforeInput}></div>
+          </li>
+          <li className={styles.dotWrapper}>
+            <div className={styles.dotBeforeInput}></div>
+          </li>
+        </ul>
+      </div>
+      <VirtualKeboard />
     </div>
   );
 }
