2f3d46783d306bb9dc77b52c7be6e7230c695433
CocoIsCat <demy176@naver.com>
2024-03-21 15:36:12 +0900

    [feat] :  가상키보드 로직 구현

--- frontend/client/src/app/_component/VirtualKeboard.tsx
+++ frontend/client/src/app/_component/VirtualKeboard.tsx
@@ -10,8 +10,28 @@ function shuffleArray(array: number[]): number[] {
   }
   return array;
 }
+interface Password {
+  value0: string;
+  value1: string;
+  value2: string;
+  value3: string;
+  value4: string;
+  value5: string;
+}
 
-export default function VirtualKeboard() {
+type Props = {
+  password: Password;
+  setPassword: React.Dispatch<React.SetStateAction<Password>>;
+  index: number;
+  setIndex: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export default function VirtualKeboard({
+  password,
+  setPassword,
+  index,
+  setIndex,
+}: Props) {
   //   const numberArrays: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   //   const numberArray = shuffleArray(numberArrays);
   const [numberArray, setNumberArray] = useState([
@@ -19,22 +39,103 @@ export default function VirtualKeboard() {
   ]);
   useEffect(() => {
     setNumberArray(shuffleArray(numberArray));
-  });
+  }, []);
+
+  const handleNumberClick = (number: number) => {
+    const update = (): number => {
+      const key = `value${index}`;
+      const updatePassword = { ...password, [key]: number };
+      setPassword(updatePassword);
+      return index + 1;
+    };
+
+    const newIndex: number = index <= 5 ? update() : index;
+    setIndex(newIndex);
+  };
+
+  // useEffect(() => {
+  //   console.log(index);
+  //   console.log(password);
+  // }, [password]);
+
+  const deleteNumberClick = () => {
+    const update = (): number => {
+      const key = `value${index - 1}`;
+      const updatePassword = { ...password, [key]: '' };
+      setPassword(updatePassword);
+      return index - 1;
+    };
+
+    const newIndex: number = index > 0 ? update() : index;
+    setIndex(newIndex);
+  };
 
   return (
     <div className={styles.keyboardContainer}>
-      <div className={styles.number}>{numberArray[0]}</div>
-      <div className={styles.number}>{numberArray[1]}</div>
-      <div className={styles.number}>{numberArray[2]}</div>
-      <div className={styles.number}>{numberArray[3]}</div>
-      <div className={styles.number}>{numberArray[4]}</div>
-      <div className={styles.number}>{numberArray[5]}</div>
-      <div className={styles.number}>{numberArray[6]}</div>
-      <div className={styles.number}>{numberArray[7]}</div>
-      <div className={styles.number}>{numberArray[8]}</div>
+      <div
+        className={styles.number}
+        onClick={() => handleNumberClick(numberArray[0])}
+      >
+        {numberArray[0]}
+      </div>
+      <div
+        className={styles.number}
+        onClick={() => handleNumberClick(numberArray[1])}
+      >
+        {numberArray[1]}
+      </div>
+      <div
+        className={styles.number}
+        onClick={() => handleNumberClick(numberArray[2])}
+      >
+        {numberArray[2]}
+      </div>
+      <div
+        className={styles.number}
+        onClick={() => handleNumberClick(numberArray[3])}
+      >
+        {numberArray[3]}
+      </div>
+      <div
+        className={styles.number}
+        onClick={() => handleNumberClick(numberArray[4])}
+      >
+        {numberArray[4]}
+      </div>
+      <div
+        className={styles.number}
+        onClick={() => handleNumberClick(numberArray[5])}
+      >
+        {numberArray[5]}
+      </div>
+      <div
+        className={styles.number}
+        onClick={() => handleNumberClick(numberArray[6])}
+      >
+        {numberArray[6]}
+      </div>
+      <div
+        className={styles.number}
+        onClick={() => handleNumberClick(numberArray[7])}
+      >
+        {numberArray[7]}
+      </div>
+      <div
+        className={styles.number}
+        onClick={() => handleNumberClick(numberArray[8])}
+      >
+        {numberArray[8]}
+      </div>
       <div className={styles.text}>취소</div>
-      <div className={styles.number}>{numberArray[9]}</div>
-      <div className={styles.text}>삭제</div>
+      <div
+        className={styles.number}
+        onClick={() => handleNumberClick(numberArray[9])}
+      >
+        {numberArray[9]}
+      </div>
+      <div className={styles.text} onClick={() => deleteNumberClick()}>
+        삭제
+      </div>
     </div>
   );
 }
