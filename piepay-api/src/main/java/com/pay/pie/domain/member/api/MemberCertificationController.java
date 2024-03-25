8d242fe1101f73f8e171baa6a7c7afd801b93579
hamseungchan <head0618@naver.com>
2024-03-25 09:11:58 +0900

    S10P22A402-212[feat] : 인증 로직 추가

--- piepay-api/src/main/java/com/pay/pie/domain/member/api/MemberCertificationController.java
+++ piepay-api/src/main/java/com/pay/pie/domain/member/api/MemberCertificationController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pay.pie.domain.member.application.MemberCertificationService;
-import com.pay.pie.domain.member.dto.request.PhoneVerificationRequest;
 import com.pay.pie.domain.member.dto.request.PhoneVerificationCheckRequest;
+import com.pay.pie.domain.member.dto.request.PhoneVerificationRequest;
 import com.pay.pie.global.common.BaseResponse;
 import com.pay.pie.global.common.code.SuccessCode;
 import com.pay.pie.global.security.dto.SecurityUserDto;
@@ -26,20 +26,20 @@ public class MemberCertificationController {
 
 	@PreAuthorize("hasAnyRole('ROLE_NOT_CERTIFIED')")
 	@PostMapping("/phone-number")
-	public void sendCertificationNumber(
-		@RequestBody PhoneVerificationRequest phoneVerificationRequest,
-		@AuthenticationPrincipal SecurityUserDto securityUserDto
+	public ResponseEntity<BaseResponse<String>> sendCertificationNumber(
+		@RequestBody PhoneVerificationRequest phoneVerificationRequest
 	) {
 		memberCertificationService.sendCertificationNumber(phoneVerificationRequest);
+		return BaseResponse.success(SuccessCode.CHECK_SUCCESS, "본인 인증이 완료되었습니다");
 	}
 
 	@PreAuthorize("hasAnyRole('ROLE_NOT_CERTIFIED')")
 	@PostMapping("/phone-number/confirm")
 	public ResponseEntity<BaseResponse<String>> confirmPhoneCertificationNumber(
-		@RequestBody PhoneVerificationCheckRequest phoneVerificationCheckRequest
+		@RequestBody PhoneVerificationCheckRequest phoneVerificationCheckRequest,
+		@AuthenticationPrincipal SecurityUserDto securityUserDto
 	) {
-
-		memberCertificationService.checkCertificationNumber(phoneVerificationCheckRequest);
+		memberCertificationService.checkCertificationNumber(phoneVerificationCheckRequest, securityUserDto);
 		return BaseResponse.success(SuccessCode.CHECK_SUCCESS, "본인 인증이 완료되었습니다");
 	}
 
