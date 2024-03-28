aa485612003b34ad0ca0f72d71fa2afce9013211
김준수 <pittuarez@naver.com>
2024-03-28 10:45:13 +0900

    S10P22A402-226[feat] : authorization route

---
+++ frontend/client/src/middleware.ts
@@ -0,0 +1,24 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+
+    if (request.nextUrl.pathname.startsWith('/login')) {
+        if (request.cookies.has('accessToken')) {
+            return NextResponse.rewrite(new URL('/', request.url));
+        }
+
+    } else if (request.nextUrl.pathname.startsWith('/success')) {
+        if (request.cookies.has('accessToken')) {
+            return NextResponse.rewrite(new URL('/', request.url));
+        }
+    } else {
+        if (!request.cookies.has('accessToken')) {
+            return NextResponse.rewrite(new URL('/login', request.url));
+        }
+    }
+}
+
+export const config = {
+    matcher: ['/:path']
+}
