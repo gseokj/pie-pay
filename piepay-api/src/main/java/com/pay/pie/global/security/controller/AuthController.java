73080f00ef330f169c779f42b342671b8f545850
hamseungchan <head0618@naver.com>
2024-03-29 16:02:04 +0900

    test

---
+++ piepay-api/src/main/java/com/pay/pie/global/security/controller/AuthController.java
@@ -0,0 +1,46 @@
+package com.pay.pie.global.security.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pay.pie.global.common.BaseResponse;
+import com.pay.pie.global.common.code.SuccessCode;
+import com.pay.pie.global.security.service.JwtService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/token")
+@RequiredArgsConstructor
+public class AuthController {
+
+	private final JwtService jwtService;
+
+	@PostMapping("/refresh")
+	public ResponseEntity<BaseResponse<String>> rotateJwtToken(@CookieValue(value = "Set-Cookie") String refreshToken) {
+		log.info(refreshToken);
+		log.info(refreshToken);
+		log.info(refreshToken);
+		log.info(refreshToken);
+		log.info(refreshToken);
+		log.info(refreshToken);
+		log.info(refreshToken);
+		log.info(refreshToken);
+		log.info(refreshToken);
+		log.info(refreshToken);
+		log.info(refreshToken);
+		log.info(refreshToken);
+		log.info(refreshToken);
+		log.info(refreshToken);
+		log.info(refreshToken);
+		log.info(refreshToken);
+		log.info(refreshToken);
+
+		return  BaseResponse.success(SuccessCode.CHECK_SUCCESS, "g2g2");
+	}
+}
