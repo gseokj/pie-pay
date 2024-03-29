44b5b03ef4a2e5004607f41fb59ffa7c959c4f46
JaeEon Hwang <hju753@naver.com>
2024-03-30 00:19:37 +0900

    S10P22A402-261[fix]: 영수증 생성시 total amount 저장

--- piepay-api/src/main/java/com/pay/pie/domain/order/entity/Order.java
+++ piepay-api/src/main/java/com/pay/pie/domain/order/entity/Order.java
@@ -16,15 +16,13 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Getter
+@Setter
 @Table(name = "`order`")
 @Builder
 // @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
--- piepay-api/src/main/java/com/pay/pie/domain/order/service/OrderService.java
+++ piepay-api/src/main/java/com/pay/pie/domain/order/service/OrderService.java
@@ -3,6 +3,7 @@ package com.pay.pie.domain.order.service;
 import java.util.List;
 import java.util.Random;
 
+import com.pay.pie.domain.orderMenu.service.OrderMenuService;
 import org.hibernate.Hibernate;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +24,7 @@ public class OrderService {
 	private final OrderRepository orderRepository;
 	private final PayRepository payRepository;
 	private final StoreRepository storeRepository;
+	private final OrderMenuService orderMenuService;
 
 	public Order save(Long payId) {
 		AddOrderRequest addOrderRequest = new AddOrderRequest();
