3de29400fb7bfd5dac09020459c4069f211f5c20
CocoisCat <demy176@naver.com>
2024-04-01 16:20:56 +0900

    [design] : MyInfo 컴포넌트 css 수정

--- frontend/client/src/styles/mypage/myInfo.css.ts
+++ frontend/client/src/styles/mypage/myInfo.css.ts
@@ -1,5 +1,5 @@
 import { globalStyle, style } from '@vanilla-extract/css';
-import theme from "@/styles/theme/theme";
+import theme from '@/styles/theme/theme';
 
 export const userInfoSection = style({
   width: '100%',
@@ -8,26 +8,35 @@ export const userInfoSection = style({
   justifyContent: 'space-between',
   padding: '0.5rem',
   backgroundColor: 'white',
-  borderRadius: '10px'
-})
+  borderRadius: '10px',
+});
 
 export const userInfoContent = style({
   display: 'flex',
   alignItems: 'center',
-
-})
+});
 
 export const userImage = style({
-  height: '3rem',
+  height: '3.5rem',
+  width: '3.5rem',
   marginRight: '10px',
   borderRadius: '7px',
-})
+});
 
 export const userInfoBox = style({
-  display: 'flex',
-  flexDirection: 'column'
-})
+  height: '100%',
 
-globalStyle(`${userInfoBox} > p`,{
-  fontWeight:'bold'
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-evenly',
+});
+
+export const cursorPointer = style({
+  display: 'flex',
+  height: '100%',
+  alignContent: 'center',
+});
+
+globalStyle(`${userInfoBox} > p`, {
+  fontWeight: 'bold',
 });
