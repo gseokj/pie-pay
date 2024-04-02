082e230926faa25c0a852268542e8b57573288f7
고석주 <hanjooseok00@gmail.com>
2024-04-02 20:49:20 +0900

    ㅁㄴㅇㄹ

--- frontend/client/src/app/(post-verification)/[meetId]/payment/select/component/MemberList.tsx
+++ frontend/client/src/app/(post-verification)/[meetId]/payment/select/component/MemberList.tsx
@@ -17,33 +17,33 @@ export default function MemberList() {
              alert("나는 방장이다");
              return;
         }
-         handleCheck(member.memberId, "isSelected")
+         handleCheck(member?.memberId, "isSelected")
     }
 
     return (
 
         <div className={styles.memberContainer}>
-            {filterMembers.filter(member => member.isFiltered)
+            {filterMembers.filter(member => member?.isFiltered)
                 .sort((a, b) => (a.isHost === b.isHost) ? 0 : a.isHost ? -1 : 1)
                 .map((member,index) => (
 
-          <div key={member.memberId} className={`${styles.container}  ${member.isSelected && styles.backgroundSkyBlue}`}>
+          <div key={member?.memberId} className={`${styles.container}  ${member?.isSelected && styles.backgroundSkyBlue}`}>
             <div className={styles.memberList}>
-                <img className={styles.image} src={member.profileImage} alt="" width={50}/>
+                <img className={styles.image} src={member?.profileImage} alt="" width={50}/>
                 <p className={styles.memberName}>
-                    {index===0 && `나 (${member.nickname})`}
-                    {index!==0 && member.nickname}
+                    {index===0 && `나 (${member?.nickname})`}
+                    {index!==0 && member?.nickname}
                 </p>
             </div>
             <button onClick={()=>onClickAgree(member,index)}>
-                {member.isSelected && !member.isTypeAlcohol && <Image className={styles.checkbox} src={check} alt="check"/>}
-                {!member.isSelected && <Image className={styles.checkbox} src={uncheck} alt="uncheck"/>}
+                {member?.isSelected && !member?.isTypeAlcohol && <Image className={styles.checkbox} src={check} alt="check"/>}
+                {!member?.isSelected && <Image className={styles.checkbox} src={uncheck} alt="uncheck"/>}
             </button>
-                <button onClick={()=>handleCheck(member.memberId, "isDrinkAlcohol")} className={styles.checkboxContainer}>
-                {member.isSelected && member.isTypeAlcohol && member.isDrinkAlcohol &&
+                <button onClick={()=>handleCheck(member?.memberId, "isDrinkAlcohol")} className={styles.checkboxContainer}>
+                {member?.isSelected && member?.isTypeAlcohol && member?.isDrinkAlcohol &&
                     <Image className={styles.checkbox} src={checkBeer} alt="uncheck"/>
                 }
-                {member.isSelected && member.isTypeAlcohol && !member.isDrinkAlcohol &&
+                {member?.isSelected && member?.isTypeAlcohol && !member?.isDrinkAlcohol &&
                     <Image className={styles.checkbox} src={uncheckBeer} alt="uncheck"/>
                 }
 
