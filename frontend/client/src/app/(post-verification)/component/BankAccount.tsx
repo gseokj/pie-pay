3c987253137840cd70d5d668e5e007d522445af0
김준수 <julsukim96@gmail.com>
2024-04-04 02:52:41 +0900

    [design] : account design

--- frontend/client/src/app/(post-verification)/component/BankAccount.tsx
+++ frontend/client/src/app/(post-verification)/component/BankAccount.tsx
@@ -3,13 +3,17 @@
 import { useEffect, useState } from 'react';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import Image from 'next/image';
-import logoHana from '@/assets/icons/logoHana.svg';
+import logoKorea from "@/assets/icons/logoKorea.svg";
+import logoKDB from "@/assets/icons/logoKDB.svg";
+import logoIBK from "@/assets/icons/logoIBK.svg";
+import logoKook from "@/assets/icons/logoKookmin.svg";
 import visibilityOff from '@/assets/icons/visibilityOff.svg';
 import visibilityOn from '@/assets/icons/visibilityOn.svg';
 import * as styles from '@/styles/main/bankAccount.css';
 import * as fontCss from '@/styles/fonts.css';
 import { Account } from '@/model/account';
 import { getCookie } from '@/util/getCookie';
+import theme from "@/styles/theme/theme";
 
 export default function BankAccount() {
   const token = getCookie('accessToken');
@@ -22,6 +26,8 @@ export default function BankAccount() {
 
   const [visibility, setVisibility] = useState(true);
   const [bankName, setBankName] = useState('은행 명');
+  const [bankColor, setBankColor] = useState(theme.korea);
+  const [bankIcon, setBankIcon] = useState(logoKorea);
 
   console.log(account);
 
@@ -30,15 +36,23 @@ export default function BankAccount() {
       switch (account[0].bankCode) {
         case '001':
           setBankName('한국은행');
+          setBankColor(theme.korea);
+          setBankIcon(logoKorea);
           break;
         case '002':
           setBankName('산업은행');
+          setBankColor(theme.kdb);
+          setBankIcon(logoKDB);
           break;
         case '003':
           setBankName('기업은행');
+          setBankColor(theme.ibk);
+          setBankIcon(logoIBK);
           break;
         case '004':
           setBankName('국민은행');
+          setBankColor(theme.kook);
+          setBankIcon(logoKook);
           break;
         default:
           setBankName('한국은행');
@@ -51,14 +65,14 @@ export default function BankAccount() {
   };
 
   return (
-    <div className={styles.bankAccountContainer}>
+    <div className={styles.bankAccountContainer} style={{ backgroundColor: bankColor }}>
       <div className={styles.bankAccountInfoContainer}>
         {typeof account !== 'undefined' && (
           <>
             <div className={styles.bankLogoContainer}>
               <Image
                 className={styles.bankLogo}
-                src={logoHana}
+                src={bankIcon}
                 height={28}
                 width={28}
                 alt="logo Hana"
