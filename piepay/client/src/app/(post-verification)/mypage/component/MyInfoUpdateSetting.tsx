09d66f72116ebe42c2ecca52000158b47d9ef86c
김준수 <pittuarez@naver.com>
2024-04-05 11:12:50 +0900

    [fix] : Log Out

--- piepay/client/src/app/(post-verification)/mypage/component/MyInfoUpdateSetting.tsx
+++ piepay/client/src/app/(post-verification)/mypage/component/MyInfoUpdateSetting.tsx
@@ -6,8 +6,16 @@ import dropUp from '@/assets/icons/dropup.svg';
 import dropDown from '@/assets/icons/dropdown.svg';
 import { useState } from 'react';
 
+function deleteCookie(name: string): void {
+  document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
+}
+
 export default function MyInfoUpdateSetting() {
   const [drop, setDrop] = useState(true);
+  const onClickLogOut = () => {
+    deleteCookie('accessToken');
+  }
+
   return (
     <section className={styles.settingSection}>
       <button
@@ -25,7 +33,7 @@ export default function MyInfoUpdateSetting() {
       {drop && (
         <div className={styles.menuList}>
           <button className={styles.menu}>간편 결제 비밀번호 변경</button>
-          <button className={styles.menu}>로그아웃</button>
+          <button className={styles.menu} onClick={onClickLogOut}>로그아웃</button>
           <button className={styles.menu}>회원 탈퇴</button>
         </div>
       )}
