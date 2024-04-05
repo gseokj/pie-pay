699790592a1a799e981919e08eb0b15aa5e7432c
김준수 <pittuarez@naver.com>
2024-04-05 10:28:11 +0900

    [fix] : middleware

--- piepay/client/src/middleware.ts
+++ piepay/client/src/middleware.ts
@@ -1,9 +1,7 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import {MyInfo} from "@/model/user";
 
 export function middleware(request: NextRequest) {
-    // console.log(request.cookies.has('accessToken'), 'in middleware......................................');
 
     if (request.nextUrl.pathname.startsWith('/kakaologin') || request.nextUrl.pathname.startsWith('/success') || request.nextUrl.pathname.startsWith('/auth')) {
         if (request.cookies.has('accessToken')) {
@@ -14,6 +12,7 @@ export function middleware(request: NextRequest) {
             return NextResponse.redirect(new URL('/kakaologin', request.url));
         }
     }
+
 }
 
 export const config = {
