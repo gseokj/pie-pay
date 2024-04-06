370adcab24a68bf84f1421a97d5881a4c3ce8160
김준수 <julsukim96@gmail.com>
2024-04-07 02:42:52 +0900

    [fix] : fix

--- piepay/client/src/app/(pre-verification)/success/page.tsx
+++ piepay/client/src/app/(pre-verification)/success/page.tsx
@@ -37,7 +37,6 @@ export default function Success({
     return (
         <section style={{width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
           <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-            <img src="@/assets/gif/piepay.gif" alt="loading gif"/>
             <Image src={piepayLoadGif} alt="loading gif" height={200} width={200} unoptimized={true} />
             <div className={fontStyles.bold} style={{ fontSize: "1.4rem" }}>로그인 완료!</div>
           </div>
