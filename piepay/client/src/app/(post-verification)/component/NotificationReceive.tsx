51aa2987c0032a6497a5abd9a72bc77239c378c7
고석주 <hanjooseok00@gmail.com>
2024-04-06 12:39:05 +0900

    [design] : modify receive notification

--- piepay/client/src/app/(post-verification)/component/NotificationReceive.tsx
+++ piepay/client/src/app/(post-verification)/component/NotificationReceive.tsx
@@ -21,10 +21,10 @@ export default function NotificationReceive() {
   const route = useRouter();
   const [isVisible, setIsVisible] = useState<boolean>(false);
   const [token, setToken] = useState('');
-  const { setEventSource,  SSEnotification, eventSource} = useSSE();
+  const { setEventSource, SSEnotification, eventSource } = useSSE();
 
   useEffect(() => {
-    if(!SSEnotification) return;
+    if (!SSEnotification) return;
     setIsVisible(prevState => !prevState);
     setTimeout(() => setIsVisible(false), 3000);
     console.log(SSEnotification);
@@ -34,33 +34,34 @@ export default function NotificationReceive() {
     const token = getCookie('accessToken') as string;
     setEventSource(token);
     setToken(token);
-    return ()=>{
-      if(eventSource) eventSource.close();
-    }
+    return () => {
+      if (eventSource) eventSource.close();
+    };
   }, [token]);
-  const handleReplace = () =>{
-      if(!SSEnotification) return;
-      if(SSEnotification.referenceId===1){
+  const handleReplace = () => {
+    if (!SSEnotification) return;
+    if (SSEnotification.referenceId === 1) {
 
-      }else if(SSEnotification.referenceId===2){
-        route.replace(`/${5}/payment/approve/${SSEnotification.destinationId}`);
-        setIsVisible(false);
-      }else if(SSEnotification.referenceId===3){
+    } else if (SSEnotification.referenceId === 2) {
+      route.replace(`/${5}/payment/approve/${SSEnotification.destinationId}`);
+      setIsVisible(false);
+    } else if (SSEnotification.referenceId === 3) {
 
-      }else if(SSEnotification.referenceId===4){
+    } else if (SSEnotification.referenceId === 4) {
 
-      }
+    }
 
-  }
+  };
 
-  return (<div onClick={handleReplace} className={`${styles.container}  ${isVisible && styles.visible}`}>
+  return (
+    <div onClick={handleReplace} className={`${styles.container}  ${isVisible && styles.visible}`}>
 
     <div className={styles.content}>
       <div className={styles.title}>
-        {SSEnotification?.referenceId === 1 && <><Image className="" src={withdraw} alt="결제"/><p>결제</p></>}
-        {SSEnotification?.referenceId === 2 && <><Image src={inroom} width={22} height={22} alt="참가"/><p>참가</p></>}
-        {SSEnotification?.referenceId === 3 && <><Image src={instead} alt="대신"/><p>대신 내주기</p></>}
-        {SSEnotification?.referenceId === 4 && <><Image src={noti} alt=""/><p>결제 알림</p></>}
+        {SSEnotification?.referenceId === 1 && <><Image className="" src={withdraw} alt="결제" /><p>결제</p></>}
+        {SSEnotification?.referenceId === 2 && <><Image src={inroom} width={22} height={22} alt="참가" /><p>참가</p></>}
+        {SSEnotification?.referenceId === 3 && <><Image src={instead} alt="대신" /><p>대신 내주기</p></>}
+        {SSEnotification?.referenceId === 4 && <><Image src={noti} alt="" /><p>결제 알림</p></>}
       </div>
       <p className={styles.paragraph}>지금</p>
     </div>
